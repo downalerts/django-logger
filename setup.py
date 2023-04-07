48dfdd4dc3a61d206a4228eb23ef89e855a7d51e
Aman Srivastav <amansrivastav.bytequests@gmail.com>
2023-04-08 00:19:56 +0530

    Added server information in middleware

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='srivastav',
-    version='0.0.2',    
+    version='0.0.3',    
     description='Downalerts django Middleware ',
     url='https://github.com/downalerts/django-logger',
     author='Aman Srivastav',
     author_email='amansrivastav.bytequests@gmail.com',
     license='BSD 2-clause',
     packages=['srivastav'],
+    install_requires=['psutil>=5.9.4'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
--- src/srivastav/middleware.py
+++ src/srivastav/middleware.py
@@ -1,36 +1,15 @@
-import platform
-import psutil
-import socket
-import os
-import requests
-
-
+from srivastav.server_info import Server
 class Middleware:
 
     def __init__(self, get_response):
         self.get_response= get_response
+        self.server = Server()
 
     def __call__(self, request):
         response= self.get_response(request)  
 
-        def bytes_to_MB(bytes):
-            mb = bytes/(1024*1024)
-            mb = round(mb, 2)
-            return mb
-        
-        physical_memory = psutil.virtual_memory()
-        disk_partitions1 = psutil.disk_partitions()
-        load1, load5, load30 = os.getloadavg()
-        load_1= round(load1, 2)
-        load_5= round(load5, 2)
-        load_30= round(load30, 2)
-
-        for partition in disk_partitions1:
-            disk_info1 = psutil.disk_usage(partition.mountpoint)
-            bytes_to_MB(disk_info1.total)
-            bytes_to_MB(disk_info1.used)
-
         api_request = {
+            "Server_Infromation": self.server.get_info(),
             "Request": {
                 "Scheme": request.scheme,
                 "Path": request.path,
@@ -45,34 +24,9 @@ class Middleware:
                 "Body": request.body,
             },
             "Response":{
-                "Headers": response.headers,
+                "Headers": response.headers if "headers" in response else response,
                 "Content": response.content
-            },
-            # "System_info":{
-            #     'technology': 'python2',
-            #     'technology_version': platform.python_version(),
-            #     'da_version': 0.1,
-            #     'da_app_name': 'python_app',
-            #     'os_name': platform.system(),
-            #     'os_version': platform.version(),
-            #     'os_arch': platform.architecture(),
-            #     'avg_load_1_min': load_1, 
-            #     'avg_load_5_min': load_5,
-            #     'avg_load_30_min': load_30,
-            #     'client_ip': socket.gethostbyname(socket.gethostname()),
-            #     'total_memory_mb': bytes_to_MB(physical_memory.total),
-            #     'used_memory_mb': bytes_to_MB(physical_memory.used),
-            #     'user_name': (os.environ.get('USERNAME')), 
-            #     'total_disk_mb': bytes_to_MB(disk_info1.total),
-            #     'used_disk_mb': bytes_to_MB(disk_info1.used),
-
-            # }
-           
+            },           
         } 
-        
-        url_enpoint="http://stage.linqer.in/"
-
-        # url_resp= requests.get(url_enpoint, params= api_request)
-        # print(url_resp.url)
         print(api_request)
         return response
